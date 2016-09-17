850abe311a277500d23af84a6339251c91f3e793
Chad Woolley <thewoolleyman@gmail.com>
2016-09-17 10:05:56 -0700

    rename refactors to make decoding less obtuse

--- EventSourcing/Decode.elm
+++ EventSourcing/Decode.elm
@@ -8,17 +8,17 @@ import Json.Decode exposing (Decoder, decodeString, succeed, fail, string, objec
 
 -- If json is coming from a port, take a Json.Decode.Value instead of String and use decodeValue instead of decodeString
 decodeEvent : String -> Event
-decodeEvent payload =
-  case decodeString eventData payload of
+decodeEvent eventPayload =
+  case decodeString eventDataDecoder eventPayload of
     Ok eventData -> { data = eventData }
     Err errorMessage -> { data = InvalidEventType errorMessage }
 
-eventData : Decoder EventData
-eventData =
-    ("eventType" := string) `andThen` decodeData
+eventDataDecoder : Decoder EventData
+eventDataDecoder =
+    ("eventType" := string) `andThen` decodeEventData
 
-decodeData :  String -> Decoder EventData
-decodeData eventType =
+decodeEventData :  String -> Decoder EventData
+decodeEventData eventType =
   case eventType of
     "TextualEntityUpdated" ->
       object1 TextualEntityUpdatedData ( "data" := string )
